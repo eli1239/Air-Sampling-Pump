aea79c0622388b7bbdf88d4b69614b287859e35a
eli1239 <68879581+eli1239@users.noreply.github.com>
2026-02-01 14:46:33 -0500

    Add files via upload

--- air_pump/air_pump.ino
+++ air_pump/air_pump.ino
@@ -1,6 +1,5 @@
 #include <avr/interrupt.h>
 #include <EEPROM.h>
-#include <Arduino.h>
 #include <Wire.h>
 #include <U8g2lib.h>
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ PIN_PC3, /* data=*/ PIN_PC2, /* reset=*/ U8X8_PIN_NONE);
